dae549fc144f36481653dbc0fccee89134a74dce
ElivertLalaj <elivertlalaj0@gmail.com>
2024-04-10 18:39:00 +0200

    Frontend Intership

--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+      showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+      function plusSlides(n) {
+        showSlides((slideIndex += n));
+      }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+      function currentSlide(n) {
+        showSlides((slideIndex = n));
+      }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
-window.addEventListener("DOMContentLoaded", function () {
-  showSlides(slideIndex);
-});
+      function showSlides(n) {
+        let i;
+        const slides = document.querySelectorAll(".carousel ul li");
+        if (n > slides.length) {
+          slideIndex = 1;
+        }
+        if (n < 1) {
+          slideIndex = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "flex";
+      }
