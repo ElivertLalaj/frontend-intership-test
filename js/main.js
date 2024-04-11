976dcd7145367aa5f5a7cd5ea6f30bab18b700f0
ElivertLalaj <elivertlalaj0@gmail.com>
2024-04-11 12:20:18 +0200

    frontend test

--- js/main.js
+++ js/main.js
@@ -23,3 +23,24 @@ let slideIndex = 1;
         }
         slides[slideIndex - 1].style.display = "flex";
       }
+
+
+      document.addEventListener("DOMContentLoaded", function() {
+        var lazyloadImages = document.querySelectorAll(".lazyload");
+    
+        var observer = new IntersectionObserver(function(entries) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    var img = entry.target;
+                    img.src = img.dataset.src;
+                    img.classList.remove("lazyload");
+                    observer.unobserve(img);
+                }
+            });
+        });
+    
+        lazyloadImages.forEach(function(img) {
+            observer.observe(img);
+        });
+    });
+    
